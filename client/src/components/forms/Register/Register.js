977d986e4fdd4e2f600df56b7cbedf2758133855
Corbin Taylor <cptaylor38@yahoo.com>
2019-06-24 18:28:37 -0400

    testing

--- client/src/components/forms/Register/Register.js
+++ client/src/components/forms/Register/Register.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './Register.css';
 
-const Register = ({ onSubmit }) => {
+const Register = ({ onSubmit, setNewUser }) => {
+
+    const returnLogin = () => {
+        setNewUser(false);
+    }
+
     return (
         <div className='registerForm container'>
             <h1>Register</h1>
@@ -15,6 +20,7 @@ const Register = ({ onSubmit }) => {
                     <input type="username" className='inputField' name="username" placeholder="Username" />
                 </div>
                 <button type="submit" style={{ marginTop: '20px' }}>Register</button>
+                <button type="button" onClick={returnLogin}>Return to Login.</button>
             </form>
         </div>
     )
--- client/src/components/trackItem/TrackItem.js
+++ client/src/components/trackItem/TrackItem.js
@@ -1,27 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import TrackModal from '../Modal/Modal';
 import useModal from '../hooks/useModal';
+import './TrackItem.css'
 
 const TrackItem = props => {
     const { isShowing, toggle } = useModal();
 
+    const refreshModals = () => {
+        document.getElementsByClassName('modalSection').empty();
+    }
+
     return (
         <React.Fragment>
             <div className='row'>
-                <div className='container'>
-                    <div className='col-lg-4 col-md-4 col-lg-12'>
-                        <img src={props.data.imageURL} alt={`Album Image`} style={{ maxHeight: '100px', maxWidth: '100px' }}></img>
-                    </div>
-                    <div className='col-lg-8 col-md-8 col-lg-12'>
+                <div className='container trackContainer'>
+                    <img src={props.data.imageURL} alt={`Album Image`} ></img>
+                    <div className='info'>
                         <h4>{props.data.name}</h4>
                         <h5>{props.data.artist}</h5>
                         <a href={props.data.albumURL} target='_blank'>Find this on spotify.</a>
-                        <button className="button-default" onClick={toggle}>Preview Song</button>
+                        <button className="button-default" onClick={toggle} onClick2={event => refreshModals()}>Preview Song</button>
                         <div className='modalSection'>
                             <TrackModal
                                 isShowing={isShowing}
                                 hide={toggle}
                                 url={props.data.url}
+                                refresh={refreshModals}
                             />
                         </div>
                     </div>
